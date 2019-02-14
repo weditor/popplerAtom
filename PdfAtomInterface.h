5a7125591b92c24dfb470c3a1cf7daf666cc7445
aochujie <weditor@163.com>
2019-02-14 10:34:52 +0800

    add comment

--- PdfAtomInterface.h
+++ PdfAtomInterface.h
@@ -32,11 +32,43 @@ public:
     explicit PdfAtomInterface(const char *pdfName, const char* ownerPW=nullptr, const char * userPW=nullptr);
     ~PdfAtomInterface();
 
+    /**! \brief is the pdf ok.
+     * \return is the pdf file ok.
+     */
     GBool isOk();
+
+    /**
+     * \return total page number of current pdf
+     */
     int getNumPages();
     void getDocInfo();
+
+    /**! \brief get all items in one page.
+     * 
+     * \param pageNum whitch page to read.
+     * \param pageInfos variable to store all items.
+     * \param scale resize the output coordinates.
+     * \return 
+     */
     void renderHtml(unsigned int pageNum, PageInfos &pageInfos, float scale=1.0);
+
+    /**! \brief get pdf tag info
+     * \return 
+     */
     std::vector<PdfStructInfo> getStructure();
+
+    /**! \brief crop rectangle area as png from pdf page,
+     * \param data data pointer to store image content
+     * \param size store the size of image returned.
+     * \param out_w width of output image
+     * \param out_h height of output image
+     * \param pageNum whitch page to crop.
+     * \param x x point of the crop rect in pdf page.
+     * \param y y point of the crop rect in pdf page.
+     * \param w width of the crop rect in pdf page.
+     * \param h height of the crop rect in pdf page.
+     * \param scale scale the output image.
+     */
     void cropImage(char **data, unsigned long* size, 
             unsigned long* out_w, unsigned long* out_h, unsigned int pageNum,
             int x=-1, int y=-1, int w=-1, int h=-1, float scale=1.0);
